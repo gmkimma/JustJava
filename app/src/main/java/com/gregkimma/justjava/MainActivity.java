daa110c02117362f1c2b876934c058256a03b9d6
gmkimma <gmkimma@yahoo.com>
2016-08-10 00:59:04 -0700

    Add nested Linear Layout

--- app/src/main/java/com/gregkimma/justjava/MainActivity.java
+++ app/src/main/java/com/gregkimma/justjava/MainActivity.java
@@ -23,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayPrice(quantity * price);
+        String priceMessage = "Total: ";
+        displayMessage(priceMessage + NumberFormat.getCurrencyInstance().format(quantity * price) + "\nThank You!");
     }
 
     /**
@@ -62,5 +63,13 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
