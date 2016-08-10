064fe2a42971a71d261c394fe8a1b221e8f8c640
gmkimma <gmkimma@yahoo.com>
2016-08-09 20:00:15 -0700

    Add price and int variable
    New method to display was added.
    Created numberOfCoffees variable.

--- app/src/main/java/com/gregkimma/justjava/MainActivity.java
+++ app/src/main/java/com/gregkimma/justjava/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        int numberOfCoffees = 1;
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees*5);
     }
 
     /**
@@ -27,5 +30,13 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
