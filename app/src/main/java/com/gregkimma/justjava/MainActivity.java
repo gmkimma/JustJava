9abecc823567b962f8617535c2ee06a185ee003d
gmkimma <gmkimma@yahoo.com>
2016-08-13 03:56:58 -0700

    Add Chocolate checkbox.

--- app/src/main/java/com/gregkimma/justjava/MainActivity.java
+++ app/src/main/java/com/gregkimma/justjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.gregkimma.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,35 +12,52 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 1;
+
+    CheckBox whippedCream;
+    CheckBox chocolate;
+
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        whippedCream = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        chocolate = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        name = (EditText) findViewById(R.id.name);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        displayMessage(createOrderSummary(quantity));
+        if (name.getText().toString().equals("")) {
+            Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show();
+        } else {
+            displayMessage(createOrderSummary());
+        }
     }
 
     /**
      * This method is called when the '+' button is clicked.
      */
     public void increment(View view) {
-        quantity++;
-        display(quantity);
+        if (quantity == 100) {
+            Toast.makeText(this, "Cannot go above 100 cups", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            quantity++;
+            display(quantity);
+        }
     }
 
     /**
      * This method is called when the '-' button is clicked.
      */
     public void decrement(View view) {
-        if (quantity == 0) {
-            Toast.makeText(MainActivity.this, "Cannot go below 0 cups", Toast.LENGTH_SHORT).show();
+        if (quantity == 1) {
+            Toast.makeText(this, "Cannot go below 1 cup", Toast.LENGTH_SHORT).show();
         }
         else {
             quantity--;
@@ -67,12 +86,23 @@ public class MainActivity extends AppCompatActivity {
      * Calculates the price of the order.
      */
     private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
+        int price = 5;
+        if (whippedCream.isChecked()) {
+            price += 1;
+        }
+        if (chocolate.isChecked()) {
+            price += 2;
+        }
+        return price * quantity;
     }
 
-    private String createOrderSummary(int quantity) {
-        String priceMessage = "Name: Greg\nQuantity: " + quantity + "\nTotal: ";
-        return priceMessage + NumberFormat.getCurrencyInstance().format(calculatePrice()) + "\nThank You!";
+    private String createOrderSummary() {
+        String priceMessage = "Name: " + name.getText();
+        priceMessage += "\nAdd whipped cream? " + whippedCream.isChecked();
+        priceMessage += "\nAdd chocolate? " + chocolate.isChecked();
+        priceMessage += "\nQuantity: " + quantity;
+        priceMessage += "\nTotal: " + NumberFormat.getCurrencyInstance().format(calculatePrice());
+        priceMessage += "\nThank You!";
+        return priceMessage;
     }
 }
