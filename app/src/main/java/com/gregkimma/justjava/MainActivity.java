9fc28ce9c8f9bfe1c9d58d675e45263f78058995
gmkimma <gmkimma@yahoo.com>
2016-08-15 02:23:09 -0700

    Add Spanish translation

--- app/src/main/java/com/gregkimma/justjava/MainActivity.java
+++ app/src/main/java/com/gregkimma/justjava/MainActivity.java
@@ -35,12 +35,12 @@ public class MainActivity extends AppCompatActivity {
         chocolate = (CheckBox) findViewById(R.id.chocolate_checkbox);
         name = (EditText) findViewById(R.id.name);
         if (name.getText().toString().equals("")) {
-            Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.enter_name_error, Toast.LENGTH_LONG).show();
         } else {
             Intent intent = new Intent(Intent.ACTION_SENDTO);
             intent.setData(Uri.parse("mailto:")); // only email apps should handle this
             intent.putExtra(Intent.EXTRA_TEXT, createOrderSummary());
-            intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for " + name.getText());
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.just_java_order) + name.getText());
             if (intent.resolveActivity(getPackageManager()) != null) {
                 startActivity(intent);
             }
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void increment(View view) {
         if (quantity == 100) {
-            Toast.makeText(this, "Cannot go above 100 cups", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, R.string.Above_100_error, Toast.LENGTH_SHORT).show();
         }
         else {
             quantity++;
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
      */
     public void decrement(View view) {
         if (quantity == 1) {
-            Toast.makeText(this, "Cannot go below 1 cup", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, R.string.Below_1_error, Toast.LENGTH_SHORT).show();
         }
         else {
             quantity--;
@@ -97,12 +97,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String createOrderSummary() {
-        String priceMessage = "Name: " + name.getText();
-        priceMessage += "\nAdd whipped cream? " + whippedCream.isChecked();
-        priceMessage += "\nAdd chocolate? " + chocolate.isChecked();
-        priceMessage += "\nQuantity: " + quantity;
-        priceMessage += "\nTotal: " + NumberFormat.getCurrencyInstance().format(calculatePrice());
-        priceMessage += "\nThank You!";
+        String priceMessage = getString(R.string.customer_name) + name.getText();
+        priceMessage += "\n" + getString(R.string.add_whipped_cream) + whippedCream.isChecked();
+        priceMessage += "\n" + getString(R.string.add_chocolate) + chocolate.isChecked();
+        priceMessage += "\n" + getString(R.string.quantity_display) + quantity;
+        priceMessage += "\n" + getString(R.string.total) + NumberFormat.getCurrencyInstance().format(calculatePrice());
+        priceMessage += "\n" + getString(R.string.thank_you);
         return priceMessage;
     }
 }
